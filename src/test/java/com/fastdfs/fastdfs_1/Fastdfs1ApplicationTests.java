9bc8e4ecc2dae69ca4af8e8b9759ad1d7bbf34a9
mayalong <515407717@qq.com>
2020-03-31 10:27:06 +0800

    fastDFS测试代码

---
+++ src/test/java/com/fastdfs/fastdfs_1/Fastdfs1ApplicationTests.java
@@ -0,0 +1,18 @@
+package com.fastdfs.fastdfs_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fastdfs1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+
+
+
+
+
+}
